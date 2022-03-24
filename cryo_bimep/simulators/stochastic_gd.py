fix how batches work in stochastic gradient descent

cryo_bimep/simulators/stochastic_gd.py
@@ -39,7 +39,8 @@ def run_stochastic_gd(
     if batch_size is None:
         batch_size = images.shape[0]
 
-    #assert images.shape[0]%batch_size == 0, "Batch size is not a multiple of the number of images"
+    number_of_batches = images.shape[0]//batch_size
+    residual_batches = images.shape[0]%batch_size
 
     sim_path = initial_path.copy()
 
@@ -55,10 +56,21 @@ def run_stochastic_gd(
 
     for _ in range(steps):
 
-        images_batch = images[np.random.randint(images.shape[0], size=batch_size), :]
+        images_shuffled = images.copy()
+        np.random.shuffle(images_shuffled)
 
-        __, grad = grad_and_energy_func(sim_path, fe_prof, images_batch, *grad_and_energy_args)
-        sim_path += -step_size*grad * mask
+        for i in range(number_of_batches):
+
+            images_batch = images_shuffled[i*batch_size:(i+1)*batch_size]
+
+            __, grad = grad_and_energy_func(sim_path, fe_prof, images_batch, *grad_and_energy_args)
+            sim_path += -step_size*grad * mask
+
+        if residual_batches != 0:
+
+            images_batch = images_shuffled[(number_of_batches-1)*batch_size:]
+            __, grad = grad_and_energy_func(sim_path, fe_prof, images_batch, *grad_and_energy_args)
+            sim_path += -step_size*grad * mask
 
         if np.sum(abs(sim_path - old_path)) < tol:
             break

example_gd.py
@@ -21,7 +21,7 @@ def main():
 
     # Set simulator
     images = np.loadtxt("example_data/2-wells_images.txt")
-    gd_steps = 100
+    gd_steps = 10
     gd_step_size = 0.00001
     gd_batch_size = int(images.shape[0]*0.1)
     gd_args = (images, gd_steps, gd_step_size, gd_batch_size)
@@ -35,7 +35,7 @@ def main():
     cb_args = (cb_sigma, cb_beta, cb_kappa)
 
     # distance constraint args
-    dc_kappa = 2000
+    dc_kappa = 200
     dc_d0 = 0.5
     dc_args = (dc_kappa, dc_d0)
 